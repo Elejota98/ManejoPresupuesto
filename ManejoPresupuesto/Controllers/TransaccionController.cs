ab96cca8338ee50253a2a759b966b5cc2b66f783
Elejota <luiscarloslealjimenez@hotmail.com>
2023-06-08 18:01:27 -0500

    TransaccionesCrear

---
+++ ManejoPresupuesto/Controllers/TransaccionController.cs
@@ -0,0 +1,54 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TransaccionController:Controller
+    {
+        private readonly IRepositorioUsuarios repositorioUsuarios;
+        private readonly IRepositorioCuentas repositorioCuentas;
+        private readonly IRepositorioCategorias repositorioCategorias;
+
+        public TransaccionController(IRepositorioUsuarios repositorioUsuarios, IRepositorioCuentas repositorioCuentas, IRepositorioCategorias repositorioCategorias)
+        {
+            this.repositorioUsuarios = repositorioUsuarios;
+            this.repositorioCuentas = repositorioCuentas;
+            this.repositorioCategorias = repositorioCategorias;
+        }
+
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = repositorioUsuarios.ObtenerUsuarios();
+            var modelo = new TransaccionCreacionViewModel();
+            modelo.Cuentas = await ObtenerCuentas(usuarioId);
+            modelo.Categorias = await ObtenerCategorias(usuarioId, modelo.TipoOperacionId);
+            return View(modelo);
+
+        }
+
+        public async Task<IEnumerable<SelectListItem>>ObtenerCuentas(int usuarioId)
+        {
+            var cuentas = await repositorioCuentas.BuscarCuentas(usuarioId);
+            return cuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+
+        }
+
+        //Listar categorias
+
+        [HttpPost]
+        public async Task<IActionResult> ObtenerCategorias([FromBody] TipoOperacion tipoOperacion)
+        {
+            var usuarioId =  repositorioUsuarios.ObtenerUsuarios();
+            var categorias = await ObtenerCategorias(usuarioId, tipoOperacion);
+            return Ok(categorias);
+        }
+
+        private async Task<IEnumerable<SelectListItem>>ObtenerCategorias(int usuarioId, TipoOperacion tipoOperacion)
+        {
+            var categorias = await repositorioCategorias.Obtener(usuarioId, tipoOperacion);
+            return categorias.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+        }
+    }
+}
