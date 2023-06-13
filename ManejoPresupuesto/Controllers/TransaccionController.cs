5eacdb3cbb9c50b6063ae0e6e70ca59068fc8f75
Elejota <luiscarloslealjimenez@hotmail.com>
2023-06-13 15:34:43 -0500

    Transacciones Editar OK

--- ManejoPresupuesto/Controllers/TransaccionController.cs
+++ ManejoPresupuesto/Controllers/TransaccionController.cs
@@ -1,7 +1,9 @@
-﻿using ManejoPresupuesto.Models;
+﻿using AutoMapper;
+using ManejoPresupuesto.Models;
 using ManejoPresupuesto.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Transactions;
 
 namespace ManejoPresupuesto.Controllers
 {
@@ -10,12 +12,22 @@ namespace ManejoPresupuesto.Controllers
         private readonly IRepositorioUsuarios repositorioUsuarios;
         private readonly IRepositorioCuentas repositorioCuentas;
         private readonly IRepositorioCategorias repositorioCategorias;
+        private readonly IRepositorioTransacciones repositorioTransacciones;
+        private readonly IMapper mapper;
 
-        public TransaccionController(IRepositorioUsuarios repositorioUsuarios, IRepositorioCuentas repositorioCuentas, IRepositorioCategorias repositorioCategorias)
+        public TransaccionController(IRepositorioUsuarios repositorioUsuarios, IRepositorioCuentas repositorioCuentas, IRepositorioCategorias repositorioCategorias,
+            IRepositorioTransacciones repositorioTransacciones, IMapper mapper)
         {
             this.repositorioUsuarios = repositorioUsuarios;
             this.repositorioCuentas = repositorioCuentas;
             this.repositorioCategorias = repositorioCategorias;
+            this.repositorioTransacciones = repositorioTransacciones;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         public async Task<IActionResult> Crear()
@@ -50,5 +62,87 @@ namespace ManejoPresupuesto.Controllers
             var categorias = await repositorioCategorias.Obtener(usuarioId, tipoOperacion);
             return categorias.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
         }
+        //CREAR
+
+        [HttpPost]
+        public async Task<IActionResult>Crear(TransaccionCreacionViewModel modelo)
+        {
+            var usuarioId = repositorioUsuarios.ObtenerUsuarios();
+            if (!ModelState.IsValid)
+            {
+                modelo.Cuentas = await ObtenerCuentas(usuarioId);
+                modelo.Categorias = await ObtenerCategorias(usuarioId, modelo.TipoOperacionId);
+                return View(modelo);
+            }
+            var cuenta = await repositorioCuentas.ObtenerPorId(modelo.CuentaId, usuarioId);
+            if (cuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            var categoria = await repositorioCategorias.ObtenerPorId(modelo.CategoriaId, usuarioId);
+            if(categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            modelo.UsuarioId = usuarioId;
+            if(modelo.TipoOperacionId== TipoOperacion.Gasto)
+            {
+                modelo.Monto *= -1;
+            }
+            await repositorioTransacciones.Crear(modelo);
+            return RedirectToAction("Index");
+        }
+        //ACTUALIZAR
+
+        [HttpGet]
+        public async Task<IActionResult>Editar(int id)
+        {
+            var usuarioId = repositorioUsuarios.ObtenerUsuarios();
+            var transaccion = await repositorioTransacciones.ObtenerPorId(id,usuarioId);
+            if(transaccion is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            var modelo = mapper.Map<TransaccionActualizacionViewModel>(transaccion);
+            modelo.MontoAnterior = modelo.Monto;
+            if (modelo.TipoOperacionId == TipoOperacion.Gasto)
+            {
+                modelo.MontoAnterior=modelo.Monto * -1;
+            }
+            modelo.CuentaAnteriorId = transaccion.CuentaId;
+            modelo.Categorias = await ObtenerCategorias(usuarioId,transaccion.TipoOperacionId);
+            modelo.Cuentas = await ObtenerCuentas(usuarioId);
+            return View(modelo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(TransaccionActualizacionViewModel modelo)
+        {
+            var usuarioId = repositorioUsuarios.ObtenerUsuarios();
+            if (!ModelState.IsValid)
+            {
+                modelo.Cuentas = await ObtenerCuentas(usuarioId);
+                modelo.Categorias = await ObtenerCategorias(usuarioId, modelo.TipoOperacionId);
+                return View(modelo);
+            }
+            var cuenta = await repositorioCuentas.ObtenerPorId(modelo.CuentaId, usuarioId);
+            if (cuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            var categorias = await repositorioCategorias.ObtenerPorId(modelo.CategoriaId, usuarioId);
+            if (categorias is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            var transaccion = mapper.Map<Transaccion>(modelo);
+            if(modelo.TipoOperacionId == TipoOperacion.Gasto)
+            {
+                transaccion.Monto *= -1;
+            }
+            await repositorioTransacciones.Actualizar(transaccion, modelo.MontoAnterior, modelo.CuentaAnteriorId);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -6,7 +6,9 @@ namespace ManejoPresupuesto.Servicios
 {
     public interface IRepositorioTransacciones
     {
+        Task Actualizar(Transaccion transaccion, decimal montoAnterior, int cuentaAnterior);
         Task Crear(Transaccion transaccion);
+        Task<Transaccion> ObtenerPorId(int id, int usuarioId);
     }
     public class RepositorioTransacciones: IRepositorioTransacciones
     {
@@ -32,6 +34,37 @@ namespace ManejoPresupuesto.Servicios
                 },
                 commandType: System.Data.CommandType.StoredProcedure);
                 transaccion.Id = id;
+        }
+
+        //Actualizar
+
+        public async Task Actualizar(Transaccion transaccion, decimal montoAnterior,
+            int cuentaAnteriorId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync("Transacciones_Actualizar",
+                new
+                {
+                    transaccion.Id,
+                    transaccion.FechaTransaccion,
+                    transaccion.Monto,
+                    transaccion.CategoriaId,
+                    transaccion.CuentaId,
+                    transaccion.Nota,
+                    montoAnterior,
+                    cuentaAnteriorId
+                }, commandType: System.Data.CommandType.StoredProcedure);
+        }
+
+        //Obtener transaccion PorId
+
+        public async Task<Transaccion> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QuerySingleOrDefaultAsync<Transaccion>($"SELECT Transacciones.*,ca.TipoOperacionId FROM Transacciones" +
+                                                                        $"  INNER JOIN Categorias ca on ca.Id = Transacciones.CategoriaId " +
+                                                                        $"WHERE Transacciones.id=@Id and Transacciones.UsuarioId=@UsuarioId",
+                                                                        new {id,usuarioId});
         }
     }
 }
